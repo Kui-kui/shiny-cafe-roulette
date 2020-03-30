b917613e3876f198a7816d2d24e04cf98d67e653
Cédric Kui <cedrickui@gmail.com>
2020-03-30 20:53:05 +0200

    chore: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    mocha: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    jest: true,
+    expect: false,
+  },
+  plugins: ['jest', 'prettier'],
+  rules: {
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/interactive-supports-focus': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-sort-default-props': 2,
+    'react/jsx-sort-props': 2,
+    'react/require-default-props': 0,
+    'react/sort-prop-types': 2,
+    'sort-keys': 2,
+  },
+}
