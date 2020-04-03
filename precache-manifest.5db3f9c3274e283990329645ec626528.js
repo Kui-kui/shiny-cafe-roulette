5864bbf2343b22a6f8df43a60b38ab62dbf8a0d4
Cédric Kui <cedrickui@gmail.com>
2020-04-03 17:42:42 +0200

    Updates

---
+++ precache-manifest.5db3f9c3274e283990329645ec626528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b247d5d73b76b1c0abb0f1610337b26",
+    "url": "/shiny-cafe-roulette/index.html"
+  },
+  {
+    "revision": "4311e5522871c63492f3",
+    "url": "/shiny-cafe-roulette/static/css/main.fbf2a2e9.chunk.css"
+  },
+  {
+    "revision": "683990c3f8d19f03f226",
+    "url": "/shiny-cafe-roulette/static/js/2.3d001a92.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/shiny-cafe-roulette/static/js/2.3d001a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4311e5522871c63492f3",
+    "url": "/shiny-cafe-roulette/static/js/main.3c7ab319.chunk.js"
+  },
+  {
+    "revision": "7b530384a0876974f831",
+    "url": "/shiny-cafe-roulette/static/js/runtime-main.cb5bf134.js"
+  }
+]);
