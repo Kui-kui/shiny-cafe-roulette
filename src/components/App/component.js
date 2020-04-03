eb7ef1df6f0b56749bfae039728dac3ad19ad6a6
Cédric Kui <cedrickui@gmail.com>
2020-04-03 13:01:18 +0200

    chore: Rearrange files

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
-import './styles.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
---
+++ src/screens/App/component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SPREADSHEET_URL =
+  'https://docs.google.com/spreadsheets/d/1W013Z2u7w4aXUoPbDYrhStEft7UGOw3AlZ31GuSlzIc/edit?usp=sharing';
+
+const App = () => {
+  return <div className="App" />;
+};
+
+export default App;
