c14a8f311a183641b94eeec15b7c414a01326c8e
Cédric Kui <cedrickui@gmail.com>
2020-04-05 17:00:11 +0200

    feat: Display cumulative scores

--- src/components/ScorePerWeek/component.js
+++ src/components/ScorePerWeek/component.js
@@ -1,39 +1,44 @@
 import PropTypes from 'prop-types';
-import { path, keys } from 'ramda';
+import { path, keys, values, sum } from 'ramda';
 import React from 'react';
 import {
-  LineChart,
+  CartesianGrid,
+  Legend,
   Line,
+  LineChart,
+  Tooltip,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
 } from 'recharts';
 
 import Styled from './styles';
+
+const getWeekIndex = (week) => week[1];
 
 const ScorePerWeek = ({ data }) => {
   const weeks = keys(path([0, 'scores'])(data));
 
   const formattedData = weeks.map((week) => {
-    const playerScores = data.reduce(
-      (acc, player) => ({
+    const playerScores = data.reduce((acc, player) => {
+      const weekIndex = getWeekIndex(week);
+      const { name, scores } = player;
+      const scoresUntilWeek = sum(values(scores).slice(0, weekIndex));
+      return {
         ...acc,
-        [player.name]: player.scores[week],
-      }),
-      {},
-    );
+        [name]: scoresUntilWeek,
+      };
+    }, {});
     return {
       week,
       ...playerScores,
     };
   });
+
   return (
     <Styled>
       <LineChart data={formattedData} height={600} width={800}>
         <XAxis dataKey="week" padding={{ left: 30, right: 30 }} />
-        <YAxis domain={[-1, 4]} minTickGap={0.5} type="number" />
+        <YAxis type="number" />
         <CartesianGrid strokeDasharray="0.1 0.1" />
         <Tooltip />
         {data.map(({ color, name }) => (
@@ -42,7 +47,7 @@ const ScorePerWeek = ({ data }) => {
             activeDot={{ r: 8 }}
             dataKey={name}
             stroke={color}
-            type="natural"
+            type="monotone"
           />
         ))}
         <Legend />
