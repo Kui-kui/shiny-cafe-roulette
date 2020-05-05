6f38faaae63a523c107f3b4644e338404c75f252
Cédric Kui <cedrickui@gmail.com>
2020-05-05 17:36:41 +0200

    Updates

--- precache-manifest.d44c4c706cbe91fcdcc83540610bd48a.js
+++ precache-manifest.d44c4c706cbe91fcdcc83540610bd48a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3ea1a438b87e70441971fcec3cdafd7",
+    "revision": "1630eba32cfaacd3253e9d70c03d15a5",
     "url": "/shiny-cafe-roulette/index.html"
   },
   {
-    "revision": "81e008e6ba744e70dad9",
+    "revision": "5d12b965866b1c285266",
     "url": "/shiny-cafe-roulette/static/css/main.fbf2a2e9.chunk.css"
   },
   {
-    "revision": "aefe947d2d1e3c6fe673",
-    "url": "/shiny-cafe-roulette/static/js/2.6abab4fa.chunk.js"
+    "revision": "0d0501880b4d6c83d39b",
+    "url": "/shiny-cafe-roulette/static/js/2.bbd679fb.chunk.js"
   },
   {
     "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
-    "url": "/shiny-cafe-roulette/static/js/2.6abab4fa.chunk.js.LICENSE.txt"
+    "url": "/shiny-cafe-roulette/static/js/2.bbd679fb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81e008e6ba744e70dad9",
-    "url": "/shiny-cafe-roulette/static/js/main.19f4e253.chunk.js"
+    "revision": "5d12b965866b1c285266",
+    "url": "/shiny-cafe-roulette/static/js/main.5bf6d2bf.chunk.js"
   },
   {
     "revision": "7b530384a0876974f831",
